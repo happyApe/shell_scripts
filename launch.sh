c16061cbd74517bbf5bb57abc3793313f08c495e
Anmol Jain <jain.anmol1119@gmail.com>
2021-11-02 15:33:44 +0530

    updates

--- launch.sh
+++ launch.sh
@@ -19,24 +19,24 @@ done
 sleep 3
 
 wmctrl -s 0 # Switches to workspace 0 
-nohup firefox &>/dev/null & disown
+nohup vivaldi-stable & disown
 sleep 3
 wmctrl -s 1 # Switches to workspace 1 
 nohup slack & disown
 sleep 2
-nohup flatpak run com.github.alainm23.planner & disown
 sleep 2
 wmctrl -s 2 # Switches to workspace 2
-nohup nautilus & disown
+nohup firefox &>/dev/null & disown
 sleep 2
 wmctrl -s 3 # Switches to workspace 3
-nohup google-chrome &>/dev/null & disown
+# nohup flatpak run com.github.alainm23.planner & disown
+nohup google-chrome-stable &>/dev/null & disown
 sleep 2
 # Moves google chrome window to workspace 3 
 # fetches window id of chrome 
 window_id=$(wmctrl -l | tail -1 | cut -f1 -d" ")
 # transfers the chrome (from fetched window id) to workspace 3
-wmctrl -i -r "$window_id" -t 3
+wmctrl -i -r "$window_id" -t 2
 sleep 2
 wmctrl -s 0 # Will go to workspace 0 after script is done executing
 exit
