44b045cd7d29fd554c3ec18c2f8e54c27834eae0
Anmol Jain <jain.anmol1119@gmail.com>
2022-08-10 14:12:22 +0530

    st updates

--- scaletorch-tmux.sh
+++ scaletorch-tmux.sh
@@ -30,20 +30,35 @@ then
     tmux new-window -t $SESSION:2 -n 'JC'
     tmux send-keys -t 'JC' "conda activate scaletorch" C-m 'clear' C-m
     tmux send-keys -t 'JC' "cd ~/ScaleTorch/jobController" C-m 'clear' C-m
+    tmux send-keys -t 'JC' "tail -f tm.log" C-m 'clear' C-m
 
-    # Splitting second window into two panes
+    # Splitting second window into 3 panes
     # Select Pane 1
     tmux selectp -t 2
-    # Splitting Pane 1 by 25 %
-    tmux splitw -v -p 25
+    # Splitting Pane 1 by 33 %
+    tmux splitw -v -p 66
     tmux send-keys -t 'JC' "conda activate scaletorch" C-m 'clear' C-m
     tmux send-keys -t 'JC' "cd ~/ScaleTorch/jobController" C-m 'clear' C-m
+    tmux send-keys -t 'JC' "tail -f mon.log" C-m 'clear' C-m
+    tmux splitw -v -p 50
+    tmux send-keys -t 'JC' "conda activate scaletorch" C-m 'clear' C-m
+    tmux send-keys -t 'JC' "cd ~/ScaleTorch/jobController" C-m 'clear' C-m
+    tmux send-keys -t 'JC' "tail -f aud.log" C-m 'clear' C-m
     tmux selectp -t 1
 
     # Create launcher window
     tmux new-window -t $SESSION:3 -n 'launcher'
     tmux send-keys -t 'launcher' "conda activate scaletorch" C-m 'clear' C-m
-    tmux send-keys -t 'launcher' "cd ~/pycodes/random_codes" C-m 'clear' C-m
+    tmux send-keys -t 'launcher' "cd ~/pycodes/examples" C-m 'clear' C-m
+
+    # Splitting the launcher window into two panes
+    # Select Pane 1
+    tmux selectp -t 1
+    # Splitting Pane 1 by 50 %
+    tmux splitw -v -p 50
+    tmux send-keys -t 'launcher' "conda activate scaletorch" C-m 'clear' C-m
+    tmux send-keys -t 'launcher' "cd ~/pycodes/examples" C-m 'clear' C-m
+    tmux selectp -t 1
 
     # Create a scaletorch window
     tmux new-window -t $SESSION:4 -n 'scaletorch'
@@ -60,9 +75,10 @@ then
     # Select Pane 1
     tmux selectp -t 0
     # Splitting Pane 1 by 10%
-    tmux splitw -v -p 10
+    tmux splitw -v -p 40
     tmux send-keys -t 'ScaleTorch' "conda activate scaletorch" C-m 'clear' C-m
     tmux send-keys -t 'ScaleTorch' "cd ~/ScaleTorch/" C-m 'clear' C-m
+
 
     # Switch to top-pane of the last window
     tmux selectp -t 1
